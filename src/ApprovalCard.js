741b77436ba84f41f46eafcf9c57605f021997bc
pjliddy <pjliddy@gmail.com>
2019-04-28 17:58:03 -0400

    Sample code on React components

---
+++ src/ApprovalCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ApprovalCard = props => {
+  const content = props.children;
+
+  return (
+    <div className="ui card">
+      <div className="content">
+        {content}
+      </div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basic green button">Approve</div>
+          <div className="ui basic red button">Reject</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ApprovalCard;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:00 PM"
+          content="Nice post"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 9:00 AM"
+          content="Great to hear"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 12:00 PM"
+          content="Great job!"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+);
